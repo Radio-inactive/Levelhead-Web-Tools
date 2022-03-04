0c28b0829416ef9bc1739bebc06593babc73f561
fhs43014 <ssantner.its-b2018@fh-salzburg.ac.at>
2022-03-04 23:25:41 +0100

    added get more button

--- Radio's Levelhead Web Tools/unplayedLevelFinder/LH_API_test.js
+++ Radio's Levelhead Web Tools/unplayedLevelFinder/LH_API_test.js
@@ -1,15 +1,16 @@
-    var levels=[];
-    var unplayedVal=false;
+    var levels = [];
+    var unplayedVal = false;
+    var fetchLimit = 128;
 
     function unplayedToggle()
     {
-        if(unplayedVal==false){
-            document.getElementById('unplayedButton').innerHTML='Unplayed only';
-            unplayedVal=true;
+        if(unplayedVal == false){
+            document.getElementById('unplayedButton').innerHTML = 'Unplayed only';
+            unplayedVal = true;
         }
         else{
-            document.getElementById('unplayedButton').innerHTML='All Levels';
-            unplayedVal=false;
+            document.getElementById('unplayedButton').innerHTML = 'All Levels';
+            unplayedVal = false;
         }
     }
 
@@ -17,21 +18,34 @@
     {
         //input from buttons
         //ToDo: make stuff not render if a text box is empty
-        var max=document.getElementById('maxEB').value;
+        var max = document.getElementById('maxEB').value;
 
-        //URL fragments
-        var URLTemplate =
-        'https://www.bscotch.net/api/levelhead/levels?marketing=true&limit=128&maxExposureBucks={{maxEB}}&includeStats=true&includeAliases=true';
 
         //assembling URL for fetch
-        return URLTemplate.replace('{{maxEB}}', max);
+        return URLTemplate.replace('{{maxEB}}', max).replace('{{limit}}', fetchLimit);
 
+    }
+
+    function createLevelCard(level){
+        var htmlout = '';
+        htmlout += levelCardTemplate
+        .replace('{{avatar}}', level.avatarId)
+        .replaceAll('{{levelId}}', level.levelId)
+        .replace('{{levelName}}', level.title)
+        .replace('{{creatorCode}}', level.userId)
+        .replace('{{alias}}', level.alias.alias)
+        .replace('{{players}}', level.stats.Players)
+        .replace('{{EB}}', level.stats.ExposureBucks)
+        .replace('{{createdAt}}', new Date(level.createdAt).toString().substring(4,15)); 
+        
+        return htmlout;
     }
 
     function fetchLevels()
     {
-        document.getElementById('levelList').innerHTML='LOADING...';
-        var htmlout="";
+        document.getElementById('getMoreButton').style.display = 'none';
+        document.getElementById('levelList').innerHTML = 'LOADING...';
+        var htmlout = "";
 
         var ref=createURL();
         console.log(ref);
@@ -43,24 +57,90 @@
         .then(
             function(r){
                 r.data.forEach(level => {
-                    if(unplayedVal==true && level.stats.Players>0);
+                    if(unplayedVal == true && level.stats.Players > 0);
                     else{
-                    htmlout+=levelCardTemplate
-                    .replace('{{avatar}}', level.avatarId)
-                    .replaceAll('{{levelId}}', level.levelId)
-                    .replace('{{levelName}}', level.title)
-                    .replace('{{creatorCode}}', level.userId)
-                    .replace('{{alias}}', level.alias.alias)
-                    .replace('{{players}}', level.stats.Players)
-                    .replace('{{EB}}', level.stats.ExposureBucks)
-                    .replace('{{createdAt}}', new Date(level.createdAt).toString().substring(4,15));  
+                        htmlout += createLevelCard(level);
                     }
 
                 });
-                console.log(r.data);
-                document.getElementById('levelList').innerHTML=htmlout;
+                levels.push(r.data)
+                console.log(levels);
+                document.getElementById('levelList').innerHTML = htmlout;
+                document.getElementById('getMoreButton').style.display = 'block';
             })
         
+    }
+
+    function removeRedundantCodes(newLevels){
+        var levelCodeList = []
+
+        if(levels != []){
+
+            levels[levels.length - 1].forEach( level => {
+                levelCodeList += level.levelId;
+            })
+            
+            for(x = 0; x < newLevels.length -1 ; x++){
+                if(levelCodeList.includes(newLevels[x].levelId))
+                    newLevels.shift()
+                else{
+                    console.log('stuff removed')
+                    break;
+                }
+                    
+            }
+            return newLevels;
+        }
+
+        
+        
+    }
+
+    function getMoreLevels(){
+
+        var url;
+        var htmlout = '';
+        
+        if(levels != []){
+
+            var newMax = levels[levels.length - 1][levels[levels.length - 1].length -1].stats.ExposureBucks;
+            console.log(newMax)
+            if(newMax != 0){
+                url = URLTemplate.replace('{{maxEB}}', newMax).replace('{{limit}}', fetchLimit);
+                console.log('fetch url getmore case 1: '+ url);
+                fetch(url)
+                .then(r => r.json())
+                .then(function(r){
+                    removeRedundantCodes(r.data);
+                    r.data.forEach( level => {
+                        htmlout += createLevelCard(level);
+                    })
+                    levels.push(r.data);
+                    console.log('filtering:')
+                    console.log(r.data);
+                    console.log(levels)
+                    document.getElementById('levelList').innerHTML += htmlout;
+                })
+            }
+            else{
+                url = URLTemplate.replace('{{maxEB}}', 0).replace('{{limit}}', fetchLimit)
+                      + "&minSecondsAgo="+ levels[levels.length - 1][levels[levels.length - 1].length -1].createdAgo;
+                console.log('fetch url getmore case 2: '+ url);
+                fetch(url)
+                .then(r => r.json())
+                .then(function(r) {
+                    removeRedundantCodes(r.data);
+                    r.data.forEach(level => {
+                        htmlout += createLevelCard(level);
+                    })
+                    levels.push(r.data);
+                    console.log('filtering:')
+                    console.log(r.data);
+                    console.log(levels)
+                    document.getElementById('levelList').innerHTML += htmlout;
+                })
+            }
+        }
     }
 
 var levelCardTemplate=`
@@ -78,3 +158,7 @@ var levelCardTemplate=`
     </div>
 </div>
 `;
+
+//URL fragments
+var URLTemplate =
+'https://www.bscotch.net/api/levelhead/levels?marketing=true&limit={{limit}}&maxExposureBucks={{maxEB}}&includeStats=true&includeAliases=true';
