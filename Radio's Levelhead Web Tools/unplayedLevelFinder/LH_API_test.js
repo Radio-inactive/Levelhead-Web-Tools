d2fef49aecb99e4538b4067baa399a8c8dc977df
fhs43014 <ssantner.its-b2018@fh-salzburg.ac.at>
2022-03-05 00:45:17 +0100

    Unplayed Level finder retooled to Advanced MD search. Daily builds load when entering the site. Minor improvements to index.

--- Radio's Levelhead Web Tools/unplayedLevelFinder/LH_API_test.js
+++ Radio's Levelhead Web Tools/unplayedLevelFinder/LH_API_test.js
@@ -2,18 +2,6 @@
     var unplayedVal = false;
     var fetchLimit = 128;
 
-    function unplayedToggle()
-    {
-        if(unplayedVal == false){
-            document.getElementById('unplayedButton').innerHTML = 'Unplayed only';
-            unplayedVal = true;
-        }
-        else{
-            document.getElementById('unplayedButton').innerHTML = 'All Levels';
-            unplayedVal = false;
-        }
-    }
-
     function createURL()
     {
         //input from buttons
@@ -27,6 +15,8 @@
     }
 
     function createLevelCard(level){
+        if(!checkFilters(level)) 
+            return '';
         var htmlout = '';
         htmlout += levelCardTemplate
         .replace('{{avatar}}', level.avatarId)
@@ -57,11 +47,7 @@
         .then(
             function(r){
                 r.data.forEach(level => {
-                    if(unplayedVal == true && level.stats.Players > 0);
-                    else{
                         htmlout += createLevelCard(level);
-                    }
-
                 });
                 levels.push(r.data)
                 console.log(levels);
@@ -81,11 +67,9 @@
             })
             
             for(x = 0; x < newLevels.length -1 ; x++){
-                if(levelCodeList.includes(newLevels[x].levelId))
-                    newLevels.shift()
-                else{
-                    console.log('stuff removed')
-                    break;
+                if(levelCodeList.includes(newLevels[x].levelId)){
+                    newLevels.splice(x, 1);
+                    --x;
                 }
                     
             }
@@ -105,7 +89,7 @@
 
             var newMax = levels[levels.length - 1][levels[levels.length - 1].length -1].stats.ExposureBucks;
             console.log(newMax)
-            if(newMax != 0){
+            if(newMax > 0 && !(newMax == levels[levels.length - 1][0].stats.ExposureBucks)){
                 url = URLTemplate.replace('{{maxEB}}', newMax).replace('{{limit}}', fetchLimit);
                 console.log('fetch url getmore case 1: '+ url);
                 fetch(url)
@@ -123,7 +107,7 @@
                 })
             }
             else{
-                url = URLTemplate.replace('{{maxEB}}', 0).replace('{{limit}}', fetchLimit)
+                url = URLTemplate.replace('{{maxEB}}', newMax).replace('{{limit}}', fetchLimit)
                       + "&minSecondsAgo="+ levels[levels.length - 1][levels[levels.length - 1].length -1].createdAgo;
                 console.log('fetch url getmore case 2: '+ url);
                 fetch(url)
@@ -143,6 +127,92 @@
         }
     }
 
+    //#region Filters
+
+    function reloadLevels(){
+        var htmlout = '';
+        document.getElementById("levelList").innerHTML = "Generating...";
+        levels.forEach( x => {
+            x.forEach(level =>{
+                htmlout += createLevelCard(level);
+            })
+        })
+        document.getElementById('levelList').innerHTML = htmlout;
+        console.log('boop')
+}
+
+    function loadTagSelect(){
+        var htmloutRequired = '<option value="0" id="noneRequired">-</option>';
+        var htmloutExcluded = '<option value="0" id="noneExcluded">-</option>';
+    
+        fetch('https://www.bscotch.net/api/levelhead/level-tags/counts')
+        .then(r => r.json())
+        .then(
+            function(r){
+                r.data.forEach( tag => {
+                    htmloutRequired += tagSelectTemplate.replaceAll('{{tagId}}', tag.tag)
+                                                        .replace('{{tagName}}', tag.name)
+                                                        .replace('{{selectName}}', 'Required');
+                    htmloutExcluded += tagSelectTemplate.replaceAll('{{tagId}}', tag.tag)
+                                                        .replace('{{tagName}}', tag.name)
+                                                        .replace('{{selectName}}', 'Excluded');
+                })
+                for(var x =1; x<4; ++x){
+                    document.getElementById('requiredTags'+ x).innerHTML = htmloutRequired.replaceAll('{{selectNumber}}', x);
+                    document.getElementById('excludedTags'+ x).innerHTML = htmloutExcluded.replaceAll('{{selectNumber}}', x);
+                }
+            }
+        )
+    }
+    
+    function showFilters(){
+        if(document.getElementById('filtersToggle').innerHTML == 'Filters ▲'){
+            document.getElementById('filtersToggle').innerHTML = 'Filters ▼';
+            document.getElementById('filters').style.display = 'block';
+        }
+        else{
+            document.getElementById('filtersToggle').innerHTML = 'Filters ▲';
+            document.getElementById('filters').style.display = 'none';
+        }
+    }
+    /* 
+    FILTERS:
+    //  Daily build: <select id="dailyFilter">
+    //  Difficulty: <select id="difficultyFilter">
+    //  Graduated?: <select id="graduationFilter">
+    //  Min Players: <input type="number" min="0" max="99999" value="0"><br>
+    //  Max Players: <input type="number" min="0" max="99999" value="999"><br>
+    //  Tower Trial: <select id="TTFilter">
+    */
+    function checkFilters(level){
+        var check = false;
+    
+        var filterVal = document.getElementById('dailyFilter').value;
+        if(!((filterVal !=0 ) ? (level.dailyBuild != (filterVal-1)) : true)) return false;
+    
+        filterVal = document.getElementById('difficultyFilter').value; 
+        if(!((filterVal != 0) ? (level.stats.Diamonds == filterVal) : true)) return false;
+    
+        //min players <= players >= max players 
+        if(!(level.stats.Players >= document.getElementById('minPlayerFilter').value
+        && level.stats.Players <= document.getElementById('maxPlayerFilter').value)) return false;
+
+        //Tag Filtering
+    
+        for(var x = 1; x < 4; ++x){
+            //check if Level has specified tags
+            if(document.getElementById('requiredTags'+ x).value != 0 ? !level.tags.includes(document.getElementById('requiredTags'+ x).value) : false) return false;
+            if(document.getElementById('excludedTags'+ x).value != 0 ? level.tags.includes(document.getElementById('excludedTags'+ x).value) : false) return false;
+        }
+    
+        return true;
+    
+    }
+
+    //#endregion
+
+
+    //#region templates
 var levelCardTemplate=`
 <div class="column">
     <div class="card">
@@ -162,3 +232,8 @@ var levelCardTemplate=`
 //URL fragments
 var URLTemplate =
 'https://www.bscotch.net/api/levelhead/levels?marketing=true&limit={{limit}}&maxExposureBucks={{maxEB}}&includeStats=true&includeAliases=true';
+
+var tagSelectTemplate=`
+<option value="{{tagId}}" id="{{tagId}}{{selectName}}{{selectNumber}}">{{tagName}}</option>
+`;
+//#endregion
